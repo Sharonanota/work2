93d166a7e76367cc9c9ae513f775e0afd1e36111
Sharonanota <matakwamatakwa@gmail.com>
2022-03-01 09:42:05 +0300

    wow

--- src/first class.kt
+++ src/first class.kt
@@ -1,27 +1,51 @@
-fun main(){
-    val x=" I am  "
-    val y=" 23 years old"
-    println(x + " " + y)
-   // var fruit= "Banana"
-   // println(fruit[0])
-   // println(fruit[1])
+fun main(){  var AkiraChix="AkiraChix"
+    println(AkiraChix[3])
+    println(AkiraChix[4])
+
+    var name="Sharon"
+    var years=1998
+    println("$name born $years")
+    var e="sharon".length
+    println(e)
+
+
+
+
+
+    }
+
+
+
+   //var fruit= "Banana"
+    //println(fruit[0])
+    //println(fruit[1])
     //println(fruit[3])
     //println(fruit[4])
     //println(fruit[5])
-    var sum=add(43,13,12)
-    println(sum)
-    var total=multiple(2,2,2)
-    println(total)
-    var u=subract(2022, 1999)
-    println(u)
+    //var sum=add(43,13,12)
+  //  println(sum)
+   // var total=multiple(2,2,2)
+   // println(total)
+  //  var u=subract(2022, 1999)
+    //println(u)
+    //var x=subraction(2022,1999)
+ //   subraction(2022, 1999)
 
-}
-fun add(a:Int ,b:Int ,c:Int){
-     println(a+b+c)
-}
-fun multiple(d:Int, e:Int, f:Int){
-    println(d+e+f)
-}
-fun subract(g:Int, h:Int){
-    println(g-h)
-}
+
+
+//}
+//fun add(a:Int ,b:Int ,c:Int){
+    // println(a+b+c)
+//}
+//fun multiple(d:Int, e:Int, f:Int){
+   // println(d+e+f)
+//}
+//fun subract(g:Int, h:Int){
+  //  println(g-h)
+//}
+//fun subraction(s:Int,n:Int){
+  //  var v=(s-n)
+    //println(v)
+    //println("I am" + " " + v + "" + "old")
+//}
+
